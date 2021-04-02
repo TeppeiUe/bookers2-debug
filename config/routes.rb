a2a9904fdd3fd0bd3956473b08c88dc72e761957
TeppeiUe <u.tetu.6125.okogo@gmail.com>
2021-04-02 07:36:11 +0000

    Initial commit

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
+  resources :relationships, only: [:create, :destroy]
   root 'homes#top'
   get 'home/about' => 'homes#about'
 end
