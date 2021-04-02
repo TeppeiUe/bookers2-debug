4a2956639a4c891c58f23a1cf8e5eeedc5f5094d
TeppeiUe <u.tetu.6125.okogo@gmail.com>
2021-04-02 01:56:16 +0000

    Add destroy action

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -7,6 +7,8 @@ class BookCommentsController < ApplicationController
     redirect_to book_path(book)
   end
   def destroy
+    BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
+    redirect_to book_path(params[:book_id])
   end
 
   private
