eefb29e3b91715a7f6324176fee042dabd41d7ef
TeppeiUe <u.tetu.6125.okogo@gmail.com>
2021-04-05 09:04:20 +0000

    Create search index

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,54 @@
 class SearchController < ApplicationController
   def search
+    @value = params[:value]
+    @model = params[:model]
+    @how = params[:how]
+    @datas = search_for(@model, @how, @value)
+  end
+
+  private
+  def match(model, value)
+    if model == 'user'
+      User.where(name: value)
+    elsif model == 'book'
+      Book.where(title: value)
+    end
+  end
+
+  def forward(model, value)
+    if model == 'user'
+      User.where("name LIKE ?", "#{value}%")
+    elsif model == 'book'
+      Book.where("title LIKE ?", "#{value}%")
+    end
+  end
+
+  def backward(model, value)
+    if model == 'user'
+      User.where("name LIKE ?", "%#{value}")
+    elsif model == 'book'
+      Book.where("title LIKE ?", "%#{value}")
+    end
+  end
+
+  def partical(model, value)
+    if model == 'user'
+      User.where("name LIKE ?", "%#{value}%")
+    elsif model == 'book'
+      Book.where("title LIKE ?", "%#{value}%")
+    end
+  end
+
+  def search_for(model, how, value)
+    case how
+    when 'match'
+      match(model, value)
+    when 'forward'
+      forward(model, value)
+    when 'backward'
+      backward(model, value)
+    when 'partical'
+      partical(model, value)
+    end
   end
 end
