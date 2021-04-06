d826328d4569e8a0f14914cab7b4f8a6996593a0
TeppeiUe <u.tetu.6125.okogo@gmail.com>
2021-04-06 17:07:55 +0000

    Solve problems that city and street autofill don't operate

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,12 +20,14 @@
 //= require_tree .
 
 $(function() {
-  return $('#user_postal_code').jpostal({
-    postcode: ['#user_postal_code'],
-    address: {
-      '#user_prefecture_code': '%3',
-      '#user_address_city': '%4',
-      '#user_address_street': '%5%6%7',
-    }
+  $(document).on('turbolinks:load', () => {
+    $('#user_postal_code').jpostal({
+      postcode: ['#user_postal_code'],
+      address: {
+        "#user_prefecture_code": '%3',
+        "#user_adress_city": '%4',
+        "#user_adress_street": '%5%6%7',
+      }
+    });
   });
 });
