e5dbdcf3f5486660bc270f950dfa007ca356b376
TeppeiUe <u.tetu.6125.okogo@gmail.com>
2021-04-06 01:33:01 +0000

    Chenge render path to index

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,4 +1,5 @@
 class BookCommentsController < ApplicationController
+
   def create
     @book = Book.find(params[:book_id])
     @book_comment = current_user.book_comments.new(book_comment_params)
@@ -6,6 +7,7 @@ class BookCommentsController < ApplicationController
     @book_comment.save
     render :index
   end
+
   def destroy
     BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
     @book = Book.find(params[:book_id])
@@ -16,4 +18,5 @@ class BookCommentsController < ApplicationController
   def book_comment_params
     params.require(:book_comment).permit(:comment)
   end
+
 end
