ed64ad1ac017d46200efd3b9d777ebd00c2b2f0e
TeppeiUe <u.tetu.6125.okogo@gmail.com>
2021-04-08 06:52:22 +0000

    Add association to models

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,10 @@ class User < ApplicationRecord
   has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   has_many :followers, through: :passive_relationships, source: :follower
 
+  has_many :chats
+  has_many :user_rooms
+  has_many :rooms, through: :user_rooms
+
   attachment :profile_image, destroy: false
 
   validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
